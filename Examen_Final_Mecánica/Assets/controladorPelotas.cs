253d222b8abc4c9180d7d03e571072a6b0c49eea
José María Beristain <josemaria_1998@outlook.com>
2021-01-11 16:01:37 -0600

    amplitud

---
+++ Examen_Final_Mecánica/Assets/controladorPelotas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorPelotas : MonoBehaviour
+{
+    public GameObject pelota;
+    public GameObject instance;
+    private float[] noiseValues;
+    private int x_trans;
+    void Start()
+    {
+        x_trans = -28;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            x_trans+=4;
+            instance=Instantiate(pelota);
+            instance.transform.position = new Vector3(x_trans, instance.transform.position.y, instance.transform.position.z);
+        }
+    }
+}
---
+++ Examen_Final_Mecánica/Assets/pelotas_rebote.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pelotas_rebote : MonoBehaviour
+{
+    public float amplitud;
+    public Rigidbody rigi;
+    public float altura_init;
+    public int rebotes;
+    private int random;
+    public float random_variable;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        random = Random.Range(0, 2);
+        random = 1;
+        rigi = gameObject.GetComponent<Rigidbody>();
+        if ( random == 1)
+        {
+            random_variable = Random.Range(0f, 20f);
+            print("amplitud");
+            transform.position = new Vector3(transform.position.x, random_variable , transform.position.z);
+        }
+        else {
+            random_variable = Random.Range(1f, 100f);
+            print("masa");
+            rigi.mass = random_variable;
+        }
+        amplitud = transform.position.y;
+        altura_init = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= altura_init) {
+            rigi.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rebotes++;
+    }
+}
