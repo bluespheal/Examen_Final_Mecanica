d0d50a6dadce0c6a04c39fdcfa7aa9c18abc23b6
Juan Felipe Bojorges <bluespheal@gmail.com>
2021-01-11 21:35:32 -0600

    Calculate vars and return with R

---
+++ Examen_Final_Mecánica/Assets/CameraReset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraReset : MonoBehaviour
+{
+    private Vector3 initialPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameObject.transform.position = initialPos;
+            gameObject.transform.parent = null;
+
+        }
+    }
+}
---
+++ Examen_Final_Mecánica/Assets/VariableWatcher.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VariableWatcher : MonoBehaviour
+{
+    private Text amplitudeText;
+    private Text speedText;
+    private Text accelerationText;
+    private Text balanceText;
+    private Text randomText;
+
+    private string amplitudeDefault;
+    private string speedDefault;
+    private string accelerationDefault;
+    private string balanceDefault;
+    private string randomDefault;
+
+    public Vector3 acceleration;
+    public Vector3 distancemoved = Vector3.zero;
+    public Vector3 lastdistancemoved = Vector3.zero;
+    public Vector3 last;
+
+    private pelotas_rebote currentSphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amplitudeText = GameObject.Find("amplitude").GetComponent<Text>();
+        speedText = GameObject.Find("speed").GetComponent<Text>();
+        accelerationText = GameObject.Find("acceleration").GetComponent<Text>();
+        balanceText = GameObject.Find("balance_point").GetComponent<Text>();
+        randomText = GameObject.Find("random_var").GetComponent<Text>();
+
+        amplitudeDefault = amplitudeText.text;
+        speedDefault = speedText.text;
+        accelerationDefault = accelerationText.text;
+        balanceDefault = balanceText.text;
+        randomDefault= randomText.text;
+    }
+
+    public void UpdateText(pelotas_rebote _currentSphere)
+    {
+        currentSphere = _currentSphere;
+        amplitudeText.text = amplitudeDefault + currentSphere.amplitud;
+        balanceText.text = balanceDefault + currentSphere.altura_init/2;
+        randomText.text = randomDefault + currentSphere.random_variable;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        speedText.text = speedDefault + currentSphere.GetComponent<Rigidbody>().velocity.magnitude;
+        accelerationText.text = accelerationDefault + CalculateAcceleration();
+    }
+
+    string CalculateAcceleration()
+    {
+        distancemoved = (transform.position - last) * Time.deltaTime;
+        acceleration = distancemoved - lastdistancemoved;
+        lastdistancemoved = distancemoved;
+        last = transform.position;
+
+        return acceleration.magnitude.ToString();
+    }
+}
