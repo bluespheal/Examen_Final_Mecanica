1abd29d0d31d46c814576cb28d48faf73239f908
BaconWarrior <urieloc.yea@gmail.com>
2021-01-11 22:48:43 -0600

    Update esferas
    Las esferas buscan su propio line renderer

--- Examen_Final_Mecánica/Assets/SpeedVector.cs
+++ Examen_Final_Mecánica/Assets/SpeedVector.cs
@@ -10,17 +10,21 @@ public class SpeedVector : MonoBehaviour
     public Transform refAbajo;
     Transform referencia;
     Color blanco;
+    Vector3 posInicial;
     public Color velocidad;
     public float velMax;
+    public string valVector;
     // Start is called before the first frame update
     private void Awake()
     {
         velMax = 20.7972f;
+        posInicial = transform.position;
     }
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         blanco = Color.yellow;
+        vecVelocidad = GameObject.Find(valVector).GetComponent<LineRenderer>();
 
     }
 
@@ -35,7 +39,7 @@ public class SpeedVector : MonoBehaviour
         {
             referencia = refArriba;
         }
-        velocidad = new Color((rb.velocity.magnitude / velMax), 0, 0, 1);
+        velocidad = new Color((rb.velocity.magnitude / velMax) + 0.1f, 0, 0, 1);
         vecVelocidad.SetColors(blanco, velocidad);
         vecVelocidad.SetPosition(0, transform.position);
         vecVelocidad.SetPosition(1, referencia.position);
