59c3815e48a8bf50cd0cf64f6162192a025c5cf9
Juan Felipe Bojorges <bluespheal@gmail.com>
2021-01-11 19:13:43 -0600

    Click to zoom to object

---
+++ Examen_Final_Mecánica/Assets/CameraFocus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocus : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject camPosition;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    void OnMouseDown()
+    {
+        Focus(camPosition);
+    }
+
+    void Focus(GameObject go)
+    {
+        cam.transform.position = go.transform.position;
+        cam.transform.parent = gameObject.transform;
+    }
+
+}
